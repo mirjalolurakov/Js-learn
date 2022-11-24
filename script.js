7fa46f2e42269d92cab7c4a7c68782480e695f1e
Mirjalol <mirgalol19@gmail.com>
2022-11-24 06:49:40 +0000

    js comleted

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ else{
 }
 
 const userName = prompt("Waht is your name?")
+
 if (userName.length % 2 == 0 ){
     console.log("Your name even, Nice!")
 }
@@ -15,17 +16,19 @@ else{
     console.log("Very bad, your name odd!")
 }
 
-const userAge = confirm("are you 18 years old?")
-if (userAge === true){
-    console.log("Then welcome!")
+const userAge = prompt('how old are you' ,'19')
+if (userAge >= 18){
+    console.log("Welcome!")
 }
 else{
 console.log("Bye man!")
 }
 
 
-const guests = "Mirziyoev Putin Trump Biden Obama Macron"
-console.log(
-    guests.includes('Obama')
+// const guests = "Mirziyoev Putin Trump Biden Obama Macron"
+// console.log(
+//     guests.includes('Obama')
 
-)
+// )
+
+
