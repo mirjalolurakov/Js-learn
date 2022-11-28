25374963b6e6659d0ac0158c619c88fcb674fe72
Mirjalol <mirgalol19@gmail.com>
2022-11-28 19:14:24 +0000

    task completed

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-const number = prompt("Enter to check number: ")
 
-if (number % 2 == 0){   
-    console.log("The number is even")
-}
-else{
-    console.log("The number is odd")
-}
+// variables
+const userName = "Mirjalol"
+const password = 7777
+const money = 10000
 
-const userName = prompt("Waht is your name?")
+// start our logic
+let userLogin = prompt("What is your name?")
 
-if (userName.length % 2 == 0 ){
-    console.log("Your name even, Nice!")
-}
-else{
-    console.log("Very bad, your name odd!")
-}
+ if(userLogin === userName){
+    let userPassword = prompt("Enter your password please")
 
-const userAge = prompt('how old are you' ,'19')
-if (userAge >= 18){
-    console.log("Welcome!")
-}
-else{
-console.log("Bye man!")
-}
+    if(userPassword){
+        let userWithdrowAmount = prompt("how much money do you want to withdraw?")
 
+        if(userWithdrowAmount){
+            let remainingMoney = money - userWithdrowAmount
+            alert('Remained money $' + remainingMoney )
+        }else{
+            alert("Error, please enter money to withdraw")
+        }
+    }else{
+        alert("Error, please enter your password")
+    }
+ }
+ else{
+   alert("Error, please enter your name")
+ }
+ 
+//  Task 2
+ let visitorName = prompt("What is your name visitor?") 
+ 
 
-// const guests = "Mirziyoev Putin Trump Biden Obama Macron"
-// console.log(
-//     guests.includes('Obama')
+ if( visitorName[0] === "A" ||  visitorName[0] ===  "a" ){
+    let ageVisitor = prompt("How old are you visitor")
+     
+    if(ageVisitor < 20 ){
+        alert('возраст подходит')
+    }
+    else if(ageVisitor =>40){
+        alert('возраст подходит')
+    }
+    else{
+        alert('Возраст не подходит')
+    }
+    let  cashVisitor = prompt("Have you cash more $100?")
+    if(cashVisitor > 100){
+        alert('У вас достаточно денег')
+    }
+    else{
+        alert('У вас мало денег')
+    }
 
-// )
-
-
+ }
+ else(
+    console.log("Error, please enter your name")
+ )
