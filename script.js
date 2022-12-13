61e1d7f2734534eb6f20705f5818196c831f68ad
Mirjalol <94471421+mirjalolurakov@users.noreply.github.com>
2022-12-13 07:35:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,131 @@
-let types = {
-   string: [],
-   number: [],
-   object: [],
-   boolean: [],
-}
+const cars = [
 
 
-bankUser = {
-   name: 'Mark',
-   age: 22,
-   street: 'Amira Timura 32',
-   married: false,
-   clever: true,
-   famaly: {
-      mother: 'Marya',
-      father: 'Jack',
-      brother: 'Linkoln',
-      sister: 'Sofya',
-      grandmother: 'Neli'
-      
+   {
+      car_name: "Amg",
+      price: 84900,
+      made: "Germany",
+      id: 9,
+      country__code: "29",
+   },
+   {
+      car_name: "Volkswagen",
+      price: 31224,
+      made: "Germany",
+      id: 9,
+      country__code: "29",
+   },
+   {
+      car_name: "Bmw",
+      price: 89032,
+      made: "Germany",
+      id: 9,
+      country__code: "29",
+   },
+   {
+      car_name: "Toyta",
+      price: 75349,
+      made: "Japan",
+      id: 9,
+      country__code: "29",
+   },
+   {
+      car_name: "Lada",
+      price: 33990,
+      made: "Russia",
+      id: 9,
+      country__code: "29",
+   },
+   {
+      car_name: "Nissan",
+      price: 29400,
+      made: "Japan",
+      id: 9,
+      country__code: "29",
+   },
+   {
+      car_name: "Laborgini",
+      price: 20,
+      made: "Italy",
+      id: 9,
+      country__code: "29",
+   },
+
+   {
+      car_name: "Ford",
+      price: 99999,
+      made: "Germany",
+      id: 9,
+      country__code: "29",
+   },
+
+   {
+      "color": ["Long Beach Blue", "Boat Tail Brown", " Amplify Orange", "Platinum Jubilee Silver", " Frozen Berry", "ow Gray", "Thundernight Metallic"]
+
    }
-}
-bankAccount = {
-   cash: [120120, 121212, 1321313, 1313133, 1313, 343444, 222],
-   credit: '12212910$',
-   investments: '21212121212122$'     
-}
-
-let newObj = Object.assign({}, bankUser, bankAccount)
-let allKeys = Object.keys(newObj)
-let allvalues = Object.values(newObj)
-let allAssigin = Object.assign({}, allKeys, allvalues)
 
 
-allAssigin.filter(valueTypes => {
-   if(typeof (valueTypes) === 'number'){
-      allAssigin.push(types.number)
-   }else if(typeof (valueTypes) === 'string'){
-       allAssigin.push(types.string)
-   }else if(typeof (valueTypes) === 'boolean'){
-            allAssigin.push(types.boolean)
-   }
-   else{
-      console.log(bye)
+]
+
+let cheap = []
+
+//forEach
+cars.forEach(car => {
+   if (car.price < 35000) {
+      cheap.push(car)
+
    }
 })
 
+console.log(cheap)
+
+// map
+// cars.map(carTwo => {
+//    if (carTwo.price < 35000) {
+//       cheap.push(carTwo)
+//    }
+// })
+
+// console.log(cheap);
+
+// cars.filter(car => {
+//    if (car.price < 35000) {
+//       cheap.push(car)
+//    }
+// })
+// console.log(cheap);
+
+// JS 4
+
+// let checkIndex = prompt('Enter')
+// let userName = ['Aleksey', 'Usman', 'Abu', 'Ali', 'Xasan']
+
+// const mapUsername = userName.map((item, index) => {
+//    if (checkIndex !== index) {
+//       userName[index]
+//    } else {
+//       userName.splice()
+//    }
+// })
+
+// console.log(mapUsername)
 
 
-console.log(allAssigin);
-console.log(types);
- 
+// userName.filter(item, index => {
+//    if (index !== userName[checkIndex]) {
+//       return userName
+//    }
+// })
 
+// console.log(userName);
 
+// // JS 5
 
+const arr = [20, 35, 44]
 
+const sum = arr.reduce(function (firstValue, secondValue) {
+   return firstValue + secondValue;
+});
+console.log(sum)
 
 
