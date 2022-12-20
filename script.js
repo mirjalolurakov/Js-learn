683b644d12ec0d5825321a2ae0320e30c9cacb5a
Mirjalol <94471421+mirjalolurakov@users.noreply.github.com>
2022-12-20 08:06:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,136 +1,143 @@
-let people = [
+let products = [
    {
-      id: Math.random(),
-      name: 'Timur',
-      info: {
-         school: '235',
-         faculity: 'SMM'
-      },
+      title: 'moloko',
+      type: 'milk',
+      price: 10000
    },
    {
-      id: Math.random(),
-      name: 'Imran',
-      info: {
-         school: 'ne izvestno',
-         faculity: 'programming'
-      },
+      title: 'potato',
+      type: 'vegetables',
+      price: 6000
    },
    {
-      id: Math.random(),
-      name: 'Aminjon',
-      info: {
-         school: '444',
-         faculity: 'Dizayn'
-      },
+      title: 'chocolate',
+      type: 'milk',
+      price: 18000
    },
    {
-      id: Math.random(),
-      name: 'Maxmud',
-      info: {
-         school: '777',
-         faculity: '3dsmax'
-      },
+      title: 'cheese',
+      type: 'milk',
+      price: 64000
    },
    {
-      id: Math.random(),
-      name: 'Muxammad',
-      info: {
-         school: '5555',
-         faculity: 'Backend'
-      },
+      title: 'pivo',
+      type: 'alcohol',
+      price: 12000
    },
    {
-      id: Math.random(),
-      name: 'Timur',
-      info: {
-         school: '235',
-         faculity: 'SMM'
-      },
+      title: 'tomato',
+      type: 'vegetables',
+      price: 17000
    },
    {
-      id: Math.random(),
-      name: 'Imran',
-      info: {
-         school: 'ne izvestno',
-         faculity: 'programming'
-      },
+      title: 'zelen',
+      type: 'vegetables',
+      price: 2500
    },
    {
-      id: Math.random(),
-      name: 'Aminjon',
-      info: {
-         school: '444',
-         faculity: 'Dizayn'
-      },
+      title: 'yogurt',
+      type: 'milk',
+      price: 11000
    },
    {
-      id: Math.random(),
-      name: 'Maxmud',
-      info: {
-         school: '777',
-         faculity: '3dsmax'
-      },
+      title: 'viski',
+      type: 'alcohol',
+      price: 540000
    },
    {
-      id: Math.random(),
-      name: 'Maxmud',
-      info: {
-         school: '777',
-         faculity: '3dsmax'
-      },
+      title: 'champane',
+      type: 'alcohol',
+      price: 70000
    },
    {
-      id: Math.random(),
-      name: 'Muxammad',
-      info: {
-         school: '5555',
-         faculity: 'Backend'
-      },
+      title: 'moloko',
+      type: 'milk',
+      price: 10000
+   },
+   {
+      title: 'potato',
+      type: 'vegetables',
+      price: 6000
+   },
+   {
+      title: 'chocolate',
+      type: 'milk',
+      price: 18000
+   },
+   {
+      title: 'cheese',
+      type: 'milk',
+      price: 64000
+   },
+   {
+      title: 'pivo',
+      type: 'alcohol',
+      price: 12000
+   },
+   {
+      title: 'tomato',
+      type: 'vegetables',
+      price: 17000
+   },
+   {
+      title: 'zelen',
+      type: 'vegetables',
+      price: 2500
+   },
+   {
+      title: 'yogurt',
+      type: 'milk',
+      price: 11000
+   },
+   {
+      title: 'viski',
+      type: 'alcohol',
+      price: 540000
+   },
+   {
+      title: 'champane',
+      type: 'alcohol',
+      price: 70000
    },
 ]
-
 
 let categories = [
    {
-      course: ' SMM',
-      count: 0
+      type: "vegetables",
+      count: 0,
+      total: 0
    },
    {
-      course: 'PROGRAMMING',
-      count: 0
+      type: "alcohol",
+      count: 0,
+      total: 0
    },
    {
-      course: '     3DSMAX',
-      count: 0
-   },
-   {
-      course: 'DIZAYN',
-      count: 0
-   },
-   {
-      course: '   BACKEND',
-      count: 0
+      type: "milk",
+      count: 0,
+      total: 0
    },
 ]
 
+// Создать категориям новый ключ arr
+// В эти ключи запушить продукты относящиеся этой же категории
+// Сортировать ключ arr по алфавиту 
 
 
+for (let item of products) {
 
+   for (let itemTwo of categories) {
+      itemTwo.newKey = []
+      if (item.type === itemTwo.type) {
+         itemTwo.count++
+         itemTwo.total += item.price
+         itemTwo.newKey.push(item.title)
 
-for (const person of people) {
-   let transformDizaynText = categories[3].course[0].toUpperCase() + categories[3].course.toLowerCase().slice(1)
-   if (person.info.faculity === categories[0].course.trim()) {
-      categories[0].count = categories[0].count + 1
-   } else if (person.info.faculity === categories[1].course.toLocaleLowerCase()) {
-      categories[1].count = categories[1].count + 1
-   } else if (person.info.faculity === categories[2].course.trim().toLowerCase()) {
-      categories[2].count = categories[2].count + 1
-   } else if (person.info.faculity === categories[3].course === transformDizaynText) {
-      categories[3].count = categories[3].count + 1
-   } else if (person.info.faculity === categories[4].course.trim().toLowerCase) {
-      categories[4].count = categories[4].count + 1
+      }
+
    }
 }
-console.log(categories)
 
+
+console.log(categories);
 
