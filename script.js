c28da4cc24ca89618a7eedb67ad9dc80b958d2dd
Mirjalol <94471421+mirjalolurakov@users.noreply.github.com>
2023-01-07 08:29:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,143 +1,23 @@
-let products = [
-   {
-      title: 'moloko',
-      type: 'milk',
-      price: 10000
-   },
-   {
-      title: 'potato',
-      type: 'vegetables',
-      price: 6000
-   },
-   {
-      title: 'chocolate',
-      type: 'milk',
-      price: 18000
-   },
-   {
-      title: 'cheese',
-      type: 'milk',
-      price: 64000
-   },
-   {
-      title: 'pivo',
-      type: 'alcohol',
-      price: 12000
-   },
-   {
-      title: 'tomato',
-      type: 'vegetables',
-      price: 17000
-   },
-   {
-      title: 'zelen',
-      type: 'vegetables',
-      price: 2500
-   },
-   {
-      title: 'yogurt',
-      type: 'milk',
-      price: 11000
-   },
-   {
-      title: 'viski',
-      type: 'alcohol',
-      price: 540000
-   },
-   {
-      title: 'champane',
-      type: 'alcohol',
-      price: 70000
-   },
-   {
-      title: 'moloko',
-      type: 'milk',
-      price: 10000
-   },
-   {
-      title: 'potato',
-      type: 'vegetables',
-      price: 6000
-   },
-   {
-      title: 'chocolate',
-      type: 'milk',
-      price: 18000
-   },
-   {
-      title: 'cheese',
-      type: 'milk',
-      price: 64000
-   },
-   {
-      title: 'pivo',
-      type: 'alcohol',
-      price: 12000
-   },
-   {
-      title: 'tomato',
-      type: 'vegetables',
-      price: 17000
-   },
-   {
-      title: 'zelen',
-      type: 'vegetables',
-      price: 2500
-   },
-   {
-      title: 'yogurt',
-      type: 'milk',
-      price: 11000
-   },
-   {
-      title: 'viski',
-      type: 'alcohol',
-      price: 540000
-   },
-   {
-      title: 'champane',
-      type: 'alcohol',
-      price: 70000
-   },
-]
+// // case 1
+// total  === 100
+// broken ===  x?
 
-let categories = [
-   {
-      type: "vegetables",
-      count: 0,
-      total: 0
-   },
-   {
-      type: "alcohol",
-      count: 0,
-      total: 0
-   },
-   {
-      type: "milk",
-      count: 0,
-      total: 0
-   },
-]
+// // case 2
+// total  === 100
+// broken? ===  x%
 
-// Создать категориям новый ключ arr
-// В эти ключи запушить продукты относящиеся этой же категории
-// Сортировать ключ arr по алфавиту 
-
-
-for (let item of products) {
-
-   for (let itemTwo of categories) {
-      itemTwo.newKey = []
-      if (item.type === itemTwo.type) {
-         itemTwo.count++
-         itemTwo.total += item.price
-         itemTwo.newKey.push(item.title)
-
-      }
-
+function calculateProportion(total, broken) {
+   if (broken.inlcudes("%")) {
+      case2
+   } else {
+      let result = (broken * 100) / total;
+      return result.toFixed(2);
    }
+
 }
+console.log(calculateProportion(1500, "23%"))
 
 
-console.log(categories);
+
+
 
