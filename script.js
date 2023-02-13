dc20f5bb61f0a38913ef72faf04bd4481eb66eae
Mirjalol <94471421+mirjalolurakov@users.noreply.github.com>
2023-02-13 06:01:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,157 +1,111 @@
-let hotel = [
-   [
-      {
-         room: 1,
-         price: 300,
-         brand: "Luxe",
-         children: false,
-      },
-      {
-         room: 2,
-         price: 4000,
-         brand: "Deluxe",
-         children: true,
-      },
-      {
-         room: 3,
-         price: 1000,
-         brand: "Deluxe",
-         children: false,
-      },
-      {
-         room: 4,
-         price: 900,
-         brand: "Luxe",
-         children: true,
-      },
-   ],
-   [
-      {
-         room: 5,
-         price: 700,
-         brand: "Luxe",
-         children: true,
-      },
-      {
-         room: 6,
-         price: 79000,
-         brand: "President",
-         children: true,
-      },
-      {
-         room: 7,
-         price: 600,
-         brand: "Luxe",
-         children: false,
-      },
-      {
-         room: 8,
-         price: 950,
-         brand: "Luxe",
-         children: true,
-      },
-   ],
-   [
-      {
-         room: 9,
-         price: 1900,
-         brand: "Deluxe",
-         children: false,
-      },
-      {
-         room: 10,
-         price: 5000,
-         brand: "Deluxe",
-         children: true,
-      },
-      {
-         room: 11,
-         price: 1000,
-         brand: "Deluxe",
-         children: false,
-      },
-      {
-         room: 12,
-         price: 250,
-         brand: "Luxe",
-         children: false,
-      },
-   ],
-   [
-      {
-         room: 13,
-         price: 10000,
-         brand: "President",
-         children: true,
-      },
-      {
-         room: 11,
-         price: 8000,
-         brand: "President",
-         children: false,
-      },
-   ],
-];
+let discount = prompt('Какая скидка?')
 
-let rooms = {
-   all_roms: [],
-   rooms: 0,
-};
-let brands = {
-   president: [],
-   deluxe: [],
-   luxe: [],
-};
+let totalSale = Number
 
-let room_level = {
-   perviy: 0,
-   vtoroy: 0,
-   tretiy: 0,
-   chetvertiy: 0,
-};
-//1 отфильтровать по категориям
-//2 добавить ключ eting с буленовым значением в каждый номер
-//3 посчитать и красиво раставить все команты переменную rooms
-//4 найти самую дорогую комнату
-//5 посчитать сколько стоит каждый этаж
-//6 посчитать сколько стоят все комнаты вместе взятые
-//7 найти все комнаты в которых живут дети
+let total = Number
 
-// Task 1 
-for (let i = 0; i < hotel.length; i++) {
-   for (let j = 0; j < hotel[i].length; j++) {
-      if (hotel[i][j].brand == 'Deluxe') {
-         brands.deluxe.push(hotel[i][j])
-      } else if (hotel[i][j].brand == 'Luxe') {
-         brands.luxe.push(hotel[i][j])
-      } else if (hotel[i][j].brand == 'President')
-         brands.president.push(hotel[i][j])
-   }
-}
+let max = Object
 
-// Task 2 
-for (let i = 0; i < hotel.length; i++) {
-   for (let j = 0; j < hotel[i].length; j++) {
-      hotel[i][j].eating = true, false;
-   }
-}
+let min = Object
 
-// Task 4
-let expensiveRoom = hotel.reduce((max, min) => max.price > min.price ? max : min);
+let average = Number
 
-//task 7 
-let allChildRooms = [];
-for (let i = 0; i < hotel.length; i++) {
-   for (let j = 0; j < hotel[i].length; j++) {
-      if (hotel[i][j].children === true) {
-         allChildRooms.push(hotel[i][j]);
-      }
 
-   }
+let arr = [
+
+    {
+
+        name: 'Milk',
+
+        price: 3.25
+
+    },
+
+    {
+
+        name: 'Coffee',
+
+        price: 1.5
+
+    },
+
+    {
+
+        name: 'Ice Cream',
+
+        price: 7.85
+
+    },
+
+    {
+
+        name: 'Tomatos',
+
+        price: 4.14
+
+    },
+
+    {
+
+        name: 'Onion',
+
+        price: 2.25
+
+    }
+
+]
+
+const setup = (arg) => {
+    // first task find max total
+
+    let maxPrice = 0
+    let maxIndex = 0
+
+    arr.filter((item) => {
+        if (item.price > maxPrice) {
+            maxPrice = item.price
+            maxIndex++
+        }
+    })
+    max = arr[maxIndex]
+
+
+    // second task find min price
+
+    let minPrice = arr[0].price
+    let minIndex = 0
+    arr.filter((item, index, arr) => {
+        if (index < arr.length - 2 && arr[index + 1].price < minPrice) {
+            minPrice = item.price
+            minIndex++
+        }
+    })
+    min = arr[minIndex]
+
+    // third task find total price
+    total = 0
+    arr.filter((item) => {
+        total += item.price
+    })
+
+    // thourt task find total Sale
+    
+    let realDiscount = 0
+    if (+discount > 0 && +discount <= 100) {
+        realDiscount = Math.round(+discount)
+    }
+    totalSale = 0
+    arr.filter((item) => {
+        totalSale += item.price * realDiscount / 100
+    })
+
+
+
+    console.log('Task 1 max price ->', max)
+    console.log('Task 2 min price ->', min)
+    console.log('Task 3 total price', total)
+    console.log('Task 5 discount', totalSale)
 }
 
 
-console.log('Brands =>', brands)
-console.log('Max room price =>', expensiveRoom)
-console.log('Rooms with children', allChildRooms)
-
+setup(arr)
