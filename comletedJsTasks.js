2ebcc5d457fda7c1d8240bd3a3714edf9edb95b1
Mirjalol <94471421+mirjalolurakov@users.noreply.github.com>
2023-03-13 10:45:22 +0000

    Update comletedJsTasks.js

--- comletedJsTasks.js
+++ comletedJsTasks.js
@@ -1356,3 +1356,32 @@
 //         }
 //     }
 //     animate();
+
+// burger 
+// const menu = document.querySelector(".menu");
+// const menuItems = document.querySelectorAll(".menuItem");
+// const hamburger = document.querySelector(".hamburger");
+// const closeIcon = document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".menuIcon");
+
+
+
+// function toggleMenu() {
+//     if (menu.classList.contains("showMenu")) { // check if element is contained
+//         menu.classList.remove("showMenu");
+//         closeIcon.style.display = "none";
+//         menuIcon.style.display = "block";
+//     } else {
+//         menu.classList.add("showMenu");
+//         closeIcon.style.display = "block";
+//         menuIcon.style.display = "none";
+//     }
+// }
+
+// hamburger.addEventListener("click", toggleMenu);
+
+
+// // iterate over each link and call
+// menuItems.forEach(function (menuItem) {
+//     menuItem.addEventListener("click", toggleMenu)
+// })
